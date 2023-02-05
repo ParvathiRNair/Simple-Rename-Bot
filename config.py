82779444deb07407f43d9cbaca71004f0f32a08d
ParvathiRNair <124461471+ParvathiRNair@users.noreply.github.com>
2023-02-05 22:11:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "10301896"))
+API_HASH = environ.get("API_HASH", "b7152dd8ebfc2ef1258600cf1c1e329c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5810250484:AAEZa9L9jW2CwXZ4DtkRdY5HLnMkDe02OAY")
+ADMIN = int(environ.get("ADMIN", "1796171542"))          
+CAPTION = environ.get("CAPTION", "@MillionXMovies")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "AgACAgUAAxkBAAMOY8UR85CFgH43q9NCkT3Hl3Wi_eEAAka0MRtxRQABVrpf4t838ZrmAAgBAAMCAAN4AAceBA")
